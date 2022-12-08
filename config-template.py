795873a7f2249d94a525354ac055c3f92786352b
Rock Chin <junyan_qin@outlook.com>
2022-12-08 14:54:17 +0800

    doc: 为配置文件添加注释

--- config-template.py
+++ config-template.py
@@ -1,3 +1,9 @@
+# Mirai的配置
+# 请到配置mirai的步骤中的教程查看每个字段的信息
+# host: 运行mirai的主机地址
+# port: 运行mirai的主机端口
+# verifyKey: mirai-api-http的verifyKey
+# qq: 机器人的QQ号
 mirai_http_api_config = {
     "host": "",
     "port": 8080,
@@ -5,6 +11,12 @@ mirai_http_api_config = {
     "qq": 0
 }
 
+# MySQL数据库的配置
+# host: 数据库地址
+# port: 数据库端口
+# user: 数据库用户名
+# password: 数据库密码
+# database: 数据库名
 mysql_config = {
     "host": "",
     "port": 3306,
@@ -13,10 +25,14 @@ mysql_config = {
     "database": ""
 }
 
+# OpenAI的配置
+# api_key: OpenAI的API Key
 openai_config = {
     "api_key": "",
 }
 
+# OpenAI的completion API的参数
+# 不了解的话请不要修改，具体请查看OpenAI的文档
 completion_api_params = {
     "model": "text-davinci-003",
     "temperature": 0.9,
@@ -26,8 +42,11 @@ completion_api_params = {
     "presence_penalty": 0.3,
 }
 
+# 消息处理的超时时间
 process_message_timeout = 45
 
+# 消息处理超时重试次数
 retry_times = 3
 
+# 每个会话的过期时间
 session_expire_time = 60 * 60 * 24 * 7
