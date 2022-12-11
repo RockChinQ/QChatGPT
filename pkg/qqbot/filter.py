d9cc39dedbfe4ecae9d792c914737033f9e7fdef
Rock Chin <junyan_qin@outlook.com>
2022-12-11 17:17:33 +0800

    feat: 添加敏感词过滤功能

---
+++ pkg/qqbot/filter.py
@@ -0,0 +1,18 @@
+import re
+
+
+class ReplyFilter:
+
+    sensitive_words = []
+
+    def __init__(self, sensitive_words: list):
+        self.sensitive_words = sensitive_words
+
+    def process(self, message: str) -> str:
+        for word in self.sensitive_words:
+            match = re.findall(word, message)
+            if len(match) > 0:
+                for i in range(len(match)):
+                    message = message.replace(match[i], "*" * len(match[i]))
+
+        return message
