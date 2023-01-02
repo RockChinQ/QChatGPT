f5bf012ba7eb55a5625c331011ab17571bf0484d
Rock Chin <1010553892@qq.com>
2023-01-02 12:56:52 +0800

    perf: 自动更新失败时提示手动更新方法

--- pkg/utils/updater.py
+++ pkg/utils/updater.py
@@ -9,4 +9,4 @@ def update_all():
     except ModuleNotFoundError:
         raise Exception("dulwich模块未安装,请查看 https://github.com/RockChinQ/QChatGPT/issues/77")
     except dulwich.porcelain.DivergedBranches:
-        raise Exception("分支不一致,自动更新仅支持master分支,请手动更新")
+        raise Exception("分支不一致,自动更新仅支持master分支,请手动更新(https://github.com/RockChinQ/QChatGPT/issues/76)")
