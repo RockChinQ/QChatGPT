b4033b29021a914675a67666777ebafa438ddcd4
Rock Chin <1010553892@qq.com>
2023-01-15 09:48:03 +0800

    debug: ExamplePlugin

--- plugins/example/__init__.py
+++ plugins/example/__init__.py
@@ -1,22 +1,23 @@
-from pkg.plugin.models import Plugin, PersonMessage, GroupMessage, register
-from pkg.plugin.host import EventContext
+import pkg.plugin.models as models
+from pkg.plugin.host import *
 
 
-@register(name="ExamplePlugin", description="用于展示QChatGPT插件支持功能的插件", version="0.0.1", author="RockChinQ")
-class ExamplePlugin(Plugin):
+@models.register(name="ExamplePlugin", description="用于展示QChatGPT插件支持功能的插件", version="0.0.1", author="RockChinQ")
+class ExamplePlugin(models.Plugin):
 
     def __init__(self):
         pass
+    #
+    # @models.on(models.PersonMessageReceived)
+    # def on_person_message_received(self, host: PluginHost, event: EventContext, **kwargs):
+    #     """收到个人消息时触发"""
+    #     host.send_person_message(kwargs['sender_id'], "你好，我是一个插件")
+    #     event.prevent_default()
 
-    @Plugin.on(PersonMessage)
-    def person_message(self, event: EventContext, **kwargs):
-        print("person_message", kwargs)
-        event.prevent_default()
-
-    @Plugin.on(GroupMessage)
-    def group_message(self, **kwargs):
-        print("group_message", kwargs)
-        self.host.notify_admin("group_message")
+    @models.on(models.KeySwitched)
+    def on_key_switched(self, host: PluginHost, event: EventContext, **kwargs):
+        """按键事件"""
+        host.send_person_message(1010553892, "按键事件")
 
     def __del__(self):
         pass
