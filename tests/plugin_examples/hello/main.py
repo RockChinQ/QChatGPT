a6bda0dec7b779c0df73f2a07e39da205a4a24d6
Rock Chin <1010553892@qq.com>
2023-01-16 19:15:02 +0800

    doc: 添加插件示例Hello

---
+++ tests/plugin_examples/hello/main.py
@@ -0,0 +1,43 @@
+from pkg.plugin.models import *
+from pkg.plugin.host import EventContext
+
+
+# 注册插件
+@register(name="hello", description="hello world", version="0.1", author="RockChinQ")
+class HelloPlugin(Plugin):
+
+    def __init__(self):
+        pass
+
+    # 当收到个人消息时触发
+    @on(PersonNormalMessageReceived)
+    def person_normal_message_received(self, event: EventContext, **kwargs):
+        msg = kwargs['text_message']
+        if msg == "hello":  # 如果消息为hello
+
+            # 输出调试信息
+            logging.debug("hello, {}".format(kwargs['sender_id']))
+
+            # 回复消息 "hello, <发送者id>!"
+            event.add_return("reply", ["hello, {}!".format(kwargs['sender_id'])])
+
+            # 阻止该事件默认行为（向接口获取回复）
+            event.prevent_default()
+
+    # 当收到群消息时触发
+    @on(GroupNormalMessageReceived)
+    def group_normal_message_received(self, event: EventContext, **kwargs):
+        msg = kwargs['text_message']
+        if msg == "hello":  # 如果消息为hello
+
+            # 输出调试信息
+            logging.debug("hello, {}".format(kwargs['sender_id']))
+
+            # 回复消息 "hello, everyone!"
+            event.add_return("reply", ["hello, everyone!"])
+
+            # 阻止该事件默认行为（向接口获取回复）
+            event.prevent_default()
+
+    def __del__(self):
+        pass
