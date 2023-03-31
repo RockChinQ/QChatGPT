82a839a60ae32771ac1a1c9589c1dcca60fa2f70
Rock Chin <1010553892@qq.com>
2023-03-31 07:06:18 +0000

    doc: 完善命令权限功能说明

--- generate_cmdpriv_template.py
+++ generate_cmdpriv_template.py
@@ -6,7 +6,7 @@ cmdsmgr.register_all()
 
 # 生成限权文件模板
 template: dict[str, int] = {
-    "comment": "以下为命令权限，请设置到cmdpriv.json中: 1为所有用户可用，2为仅管理员可用。",
+    "comment": "以下为命令权限，请设置到cmdpriv.json中。关于此功能的说明，请查看：https://github.com/RockChinQ/QChatGPT/wiki/%E5%8A%9F%E8%83%BD%E4%BD%BF%E7%94%A8#%E5%91%BD%E4%BB%A4%E6%9D%83%E9%99%90%E6%8E%A7%E5%88%B6",
 }
 
 for key in cmdsmgr.__command_list__:
