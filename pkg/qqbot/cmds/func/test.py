8c41e3506f5af65019c851528cfac3ea04301e16
Rock Chin <1010553892@qq.com>
2023-03-31 07:22:33 +0000

    test: 测试工作流

---
+++ pkg/qqbot/cmds/func/test.py
@@ -0,0 +1,20 @@
+from ..mgr import AbstractCommandNode, Context
+
+
+@AbstractCommandNode.register(
+    parent=None,
+    name="test",
+    description="测试指令",
+    usage="!test",
+    aliases=[],
+    privilege=0
+)
+class TestCommand(AbstractCommandNode):
+    @classmethod
+    def process(cls, ctx: Context) -> tuple[bool, list]:
+        reply = []
+            
+        reply.append('测试指令')
+
+        return True, reply
+    
