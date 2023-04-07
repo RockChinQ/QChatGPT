6068ab710063956d25956f99bf03be7bf82743ac
Rock Chin <1010553892@qq.com>
2023-04-07 16:39:25 +0800

    feat: 修改help_message为主线的内容

--- tips-custom-template.py
+++ tips-custom-template.py
@@ -18,15 +18,9 @@ rate_limit_drop_tip = "本分钟对话次数超过限速次数，此对话被丢
 # 指令！help帮助消息
 # config.py,line:279
 # pkg/qqbot/process.py,line:122
-help_message = """【可自定义】
-此机器人通过调用OpenAI的GPT-3大型语言模型生成回复，不具有情感。
+help_message = """此机器人通过调用大型语言模型生成回复，不具有情感。
 你可以用自然语言与其交流，回复的消息中[GPT]开头的为模型生成的语言，[bot]开头的为程序提示。
-了解此项目请找QQ 1010553892 联系作者
-请不要用其生成整篇文章或大段代码，因为每次只会向模型提交少部分文字，生成大部分文字会产生偏题、前后矛盾等问题
-每次会话最后一次交互后{}分钟后会自动结束，结束后将开启新会话，如需继续前一次会话请发送 !last 重新开启
-欢迎到github.com/RockChinQ/QChatGPT 给个star
-指令帮助信息请查看: https://github.com/RockChinQ/QChatGPT/wiki/%E5%8A%9F%E8%83%BD%E4%BD%BF%E7%94%A8#%E6%9C%BA%E5%99%A8%E4%BA%BA%E6%8C%87%E4%BB%A4""".format(
-    config.session_expire_time // 60)
+欢迎到github.com/RockChinQ/QChatGPT 给个star"""
 
 # ---原config.py结束--
 
