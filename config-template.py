cef70751ff9680058315049d3ae6c8d6ccf2cdd6
Rock Chin <1010553892@qq.com>
2023-06-16 19:35:06 +0800

    chore: 修改配置文件说明

--- config-template.py
+++ config-template.py
@@ -204,11 +204,13 @@ auto_reset = True
 # 现已支持的模型有：
 # 
 #    'gpt-4'
-#    'gpt-4-0314'
+#    'gpt-4-0613'
 #    'gpt-4-32k'
-#    'gpt-4-32k-0314'
+#    'gpt-4-32k-0613'
 #    'gpt-3.5-turbo'
-#    'gpt-3.5-turbo-0301'
+#    'gpt-3.5-turbo-16k'
+#    'gpt-3.5-turbo-0613'
+#    'gpt-3.5-turbo-16k-0613'
 #    'text-davinci-003'
 #    'text-davinci-002'
 #    'code-davinci-002'
