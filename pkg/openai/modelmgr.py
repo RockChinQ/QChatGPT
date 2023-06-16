ddb211b74a1aa11d63e5429774d28f8091fd2a5d
Rock Chin <1010553892@qq.com>
2023-06-16 19:35:26 +0800

    feat: 支持新的模型

--- pkg/openai/modelmgr.py
+++ pkg/openai/modelmgr.py
@@ -20,11 +20,14 @@ COMPLETION_MODELS = {
 
 CHAT_COMPLETION_MODELS = {
     'gpt-3.5-turbo',
-    'gpt-3.5-turbo-0301',
+    'gpt-3.5-turbo-16k',
+    'gpt-3.5-turbo-0613',
+    'gpt-3.5-turbo-16k-0613',
+    # 'gpt-3.5-turbo-0301',
     'gpt-4',
-    'gpt-4-0314',
+    'gpt-4-0613',
     'gpt-4-32k',
-    'gpt-4-32k-0314'
+    'gpt-4-32k-0613'
 }
 
 EDIT_MODELS = {
