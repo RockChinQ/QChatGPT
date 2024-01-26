411034902aadb916f651263fbb211cfe98bcb383
RockChinQ <1010553892@qq.com>
2024-01-27 00:05:55 +0800

    feat: 启动时展示asciiart

--- start.py
+++ start.py
@@ -1,7 +1,15 @@
 import asyncio
 
-from pkg.core import boot
 
+asciiart = r"""
+  ___   ___ _         _    ___ ___ _____ 
+ / _ \ / __| |_  __ _| |_ / __| _ \_   _|
+| (_) | (__| ' \/ _` |  _| (_ |  _/ | |  
+ \__\_\\___|_||_\__,_|\__|\___|_|   |_|  
+"""
 
 if __name__ == '__main__':
+    print(asciiart)
+
+    from pkg.core import boot
     asyncio.run(boot.main())
