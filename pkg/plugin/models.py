c75b0ce8fb21aa669a580f9ffb480a360fd089e0
RockChinQ <1010553892@qq.com>
2024-01-29 21:31:11 +0800

    perf: 优化代码声明

--- pkg/plugin/models.py
+++ pkg/plugin/models.py
@@ -1,1 +1,26 @@
+from __future__ import annotations
+
+import typing
+
 from .context import BasePlugin as Plugin
+from . import events
+
+def register(
+    name: str,
+    description: str,
+    version: str,
+    author
+) -> typing.Callable[[typing.Type[Plugin]], typing.Type[Plugin]]:
+    pass
+
+
+def on(
+    event: typing.Type[events.BaseEventModel]
+) -> typing.Callable[[typing.Callable], typing.Callable]:
+    pass
+
+
+def func(
+    name: str=None,
+) -> typing.Callable:
+    pass
