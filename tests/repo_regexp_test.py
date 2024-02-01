0dec10ddf2e8b5c241cd3fb65d756e9cbfa68300
RockChinQ <1010553892@qq.com>
2024-02-01 18:38:04 +0800

    chore: 删除tests目录

--- tests/repo_regexp_test.py
+++
@@ -1,7 +0,0 @@
-import re
-
-repo_url = "git@github.com:RockChinQ/WebwlkrPlugin.git"
-
-repo =  re.findall(r'(?:https?://github\.com/|git@github\.com:)([^/]+/[^/]+?)(?:\.git|/|$)', repo_url)
-
-print(repo)
