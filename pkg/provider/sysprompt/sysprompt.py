13393b66242519ab385075264d923ee98bb3df49
Junyan Qin <1010553892@qq.com>
2024-03-12 16:31:54 +0000

    feat: 限速算法的扩展性

--- pkg/provider/sysprompt/sysprompt.py
+++ pkg/provider/sysprompt/sysprompt.py
@@ -22,14 +22,16 @@ class PromptManager:
 
         mode_name = self.ap.provider_cfg.data['prompt-mode']
 
+        loader_class = None
+
         for loader_cls in loader.preregistered_loaders:
             if loader_cls.name == mode_name:
-                loader_cls = loader_cls
+                loader_class = loader_cls
                 break
         else:
             raise ValueError(f'未知的 Prompt 加载器: {mode_name}')
 
-        self.loader_inst: loader.PromptLoader = loader_cls(self.ap)
+        self.loader_inst: loader.PromptLoader = loader_class(self.ap)
 
         await self.loader_inst.initialize()
         await self.loader_inst.load()
